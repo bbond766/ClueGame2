77cbac6a3022b509104f07349ccd9cc36da20ea0
Kaveman <kehayes@mymail.mines.edu>
2015-10-06 23:32:28 -0600

    This is the skeleton of Part 1 failing tests.  I added the two board classes and the jUnit class.  I also inserted the required functions or methods and general declarasions of maps and sets,  I need to review data structures for a minute.

---
+++ Clue_Game/src/experiment/IntBoard.java
@@ -0,0 +1,28 @@
+package experiment;
+
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+
+public class IntBoard {
+	private Map<BoardCell, LinkedList<BoardCell>> adjMtx;
+	private Set<BoardCell> visited;
+	private Set<BoardCell> targets;
+	private BoardCell[][] grid;
+	
+	public void calcAdjacencies(){
+		
+	}
+	
+	public void calcTargets(){
+		
+	}
+	
+	public void getTargets(){
+		
+	}
+	
+	public void getAdjList(){
+		
+	}
+}
