e0fcfcdc84c984f5b4f61e3219a9048b7a0ea90a
unknown <jostockw@MZ022-54.adit.mines.edu>
2015-10-08 11:18:40 -0600

    Added to skeleton

--- Clue_Game/src/experiment/IntBoardTest.java
+++ Clue_Game/src/experiment/IntBoardTest.java
@@ -1,13 +1,38 @@
 package experiment;
 
 import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class IntBoardTest {
 
+	@Before
+	public void init() {
+		IntBoard board = new IntBoard();
+	}
+	
 	@Test
-	public void test() {
+	public void topLeftCorner() {
+		fail("Not yet implemented");
+	}
+	
+	@Test public void bottomRightCorner() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void rightEdge() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void leftEdge() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void secondColumn() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void secondFromLastColumn() {
 		fail("Not yet implemented");
 	}
 
